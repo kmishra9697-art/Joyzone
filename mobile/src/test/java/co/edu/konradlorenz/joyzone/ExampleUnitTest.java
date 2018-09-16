8a29a3f1c43de8d2a20521aa56358a18fc00b45f
Dreivko <jorgesebasah@hotmail.com>
2018-09-16 14:28:49 -0500

    Fix Codacy errors part 2

--- mobile/src/test/java/co/edu/konradlorenz/joyzone/ExampleUnitTest.java
+++ mobile/src/test/java/co/edu/konradlorenz/joyzone/ExampleUnitTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
+    public void Addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
 }
