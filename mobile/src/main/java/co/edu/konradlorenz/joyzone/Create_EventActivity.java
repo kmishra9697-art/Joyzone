4dbca3caf517c6a147c176a6f44514f00a2c9198
Dreivko <jorgesebasah@hotmail.com>
2018-11-18 18:47:21 -0500

    Validations on Create Event Activity

--- mobile/src/main/java/co/edu/konradlorenz/joyzone/Create_EventActivity.java
+++ mobile/src/main/java/co/edu/konradlorenz/joyzone/Create_EventActivity.java
@@ -13,6 +13,10 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.custom.SimpleCustomValidation;
+import com.google.common.collect.Range;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,13 +24,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 import co.edu.konradlorenz.joyzone.Adapters.EventAdapter;
 import co.edu.konradlorenz.joyzone.entities.EventEntity;
 import co.edu.konradlorenz.joyzone.entities.UserEntity;
 
 public class Create_EventActivity extends AppCompatActivity {
+
+    AwesomeValidation mAwesomeValidation;
 
     FloatingActionButton fab;
     String user;
@@ -36,6 +46,10 @@ public class Create_EventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create__event);
+
+        mAwesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+
         user = getIntent().getExtras().getString("user");
 
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -62,6 +76,30 @@ public class Create_EventActivity extends AppCompatActivity {
         final EditText descripcion = (EditText) findViewById(R.id.descripcion);
         final EditText cantidad = (EditText) findViewById(R.id.Capacidad);
 
+        mAwesomeValidation.addValidation(Create_EventActivity.this, R.id.Nombre, "[a-zA-Z\\s]+", R.string.error_event_name);
+        mAwesomeValidation.addValidation(Create_EventActivity.this, R.id.Fecha,new SimpleCustomValidation() {
+            @Override
+            public boolean compare(String input) {
+                // check if the Event Date is upper
+                try {
+                    Calendar calendarEvent = Calendar.getInstance();
+                    Calendar calendarToday = Calendar.getInstance();
+                    calendarEvent.setTime(new SimpleDateFormat("dd/MM/yyyy", Locale.US).parse(input));
+                    int yearOfToday = calendarToday.get(Calendar.YEAR);
+                    int yearOfEvent = calendarEvent.get(Calendar.YEAR);
+                        if (yearOfToday >= yearOfEvent ) {
+                            return true;
+                        }else{
+                            return false;
+                        }
+                } catch(ParseException e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+            },R.string.error_event_date);
+
+        mAwesomeValidation.addValidation(Create_EventActivity.this, R.id.Capacidad, Range.closed(1,1000000), R.string.error_event_capacity);
 
 
         boton.setOnClickListener(new View.OnClickListener() {
@@ -109,10 +147,11 @@ public class Create_EventActivity extends AppCompatActivity {
                 DatabaseReference childRef = myRef2.push();
 
                 childRef.setValue(userdata);
-
-                Intent intento = new Intent(Create_EventActivity.this, MainActivity.class);
-                intento.putExtra("user",user);
-                startActivity(intento);
+                if (mAwesomeValidation.validate()){
+                    Intent intento = new Intent(Create_EventActivity.this, MainActivity.class);
+                    intento.putExtra("user",user);
+                    startActivity(intento);
+                }
             }
         });
     }
